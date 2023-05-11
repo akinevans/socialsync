3489eb9afdacd1952a24266cb24d8964ea941e5a
Akin Evans <akinevans@gmail.com>
2023-05-11 10:24:40 -0700

    Build Button White & Orange Components

---
+++ src/components/Button_Orange.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Button.css";
+
+export default function Button_Orange(props) {
+  return <button className='btn btn-orange'>{props.title}</button>;
+}
---
+++ src/components/Button_White.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Button.css";
+
+export default function Button_White(props) {
+  return <button className='btn btn-white'>{props.title}</button>;
+}
