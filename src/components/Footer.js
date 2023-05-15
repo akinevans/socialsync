7ffbfc9cc40529f57623c052abd3b7a616dfe43d
Akin Evans <akinevans@gmail.com>
2023-05-15 09:47:21 -0700

    Implement responsive mobile view for Footer, Footer_Link, and Footer_Subscription components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,28 +8,32 @@ export default function Footer() {
   return (
     <div className='footer-outer-wrapper'>
       <div className='footer-inner-wrapper'>
-        <Footer_Subscription />
-        <Footer_Links
-          title='Features'
-          link1='Business Messenger'
-          link2='Customizable Bots'
-          link3='Automated Answers'
-          link4='Product Tours'
-        />
-        <Footer_Links
-          title='Resources'
-          link1='Blog'
-          link2='Watch a Demo'
-          link3='Academy'
-          link4='Webinars'
-        />
-        <Footer_Links
-          title='Company'
-          link1='About'
-          link2='Customers'
-          link3='Community Forum'
-          link4='Careers'
-        />
+        <div className='footer-sub-component-wrapper'>
+          <Footer_Subscription />
+        </div>
+        <div className='footer-links-component-wrapper'>
+          <Footer_Links
+            title='Features'
+            link1='Business Messenger'
+            link2='Customizable Bots'
+            link3='Automated Answers'
+            link4='Product Tours'
+          />
+          <Footer_Links
+            title='Resources'
+            link1='Blog'
+            link2='Watch a Demo'
+            link3='Academy'
+            link4='Webinars'
+          />
+          <Footer_Links
+            title='Company'
+            link1='About'
+            link2='Customers'
+            link3='Community Forum'
+            link4='Careers'
+          />
+        </div>
       </div>
       <div className='credit-wrapper'>
         <a href='https://dribbble.com/hasan_ui_design' target='_blank'>
